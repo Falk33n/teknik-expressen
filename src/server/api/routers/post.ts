ab6f422345667e6ce7e4a3c30ed8b2044fc00be0
Falk33n <tim.falk00@gmail.com>
2024-10-27 10:41:39 +0100

    initial setup

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,6 +1,5 @@
-import { z } from "zod";
-
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
+import { z } from 'zod';
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -23,7 +22,7 @@ export const postRouter = createTRPCRouter({
 
   getLatest: publicProcedure.query(async ({ ctx }) => {
     const post = await ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
+      orderBy: { createdAt: 'desc' },
     });
 
     return post ?? null;
--- start-database.sh
+++ start-database.sh
@@ -1,14 +1,3 @@
-#!/usr/bin/env bash
-# Use this script to start a docker container for a local development database
-
-# TO RUN ON WINDOWS:
-# 1. Install WSL (Windows Subsystem for Linux) - https://learn.microsoft.com/en-us/windows/wsl/install
-# 2. Install Docker Desktop for Windows - https://docs.docker.com/docker-for-windows/install/
-# 3. Open WSL - `wsl`
-# 4. Run this script - `./start-database.sh`
-
-# On Linux and macOS you can run this script directly - `./start-database.sh`
-
 DB_CONTAINER_NAME="teknik-expressen-mysql"
 
 if ! [ -x "$(command -v docker)" ]; then
@@ -27,7 +16,6 @@ if [ "$(docker ps -q -a -f name=$DB_CONTAINER_NAME)" ]; then
   exit 0
 fi
 
-# import env variables from .env
 set -a
 source .env
 
@@ -41,7 +29,6 @@ if [ "$DB_PASSWORD" == "password" ]; then
     echo "Please change the default password in the .env file and try again"
     exit 1
   fi
-  # Generate a random URL-safe password
   DB_PASSWORD=$(openssl rand -base64 12 | tr '+/' '-_')
   sed -i -e "s#:password@#:$DB_PASSWORD@#" .env
 fi
