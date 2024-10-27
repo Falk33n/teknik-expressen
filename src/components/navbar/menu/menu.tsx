8f12d53b46dde9c60f54d0f7a8df4766fcbfe4f0
Falk33n <tim.falk00@gmail.com>
2024-10-27 20:52:39 +0100

    added accordion

--- src/components/navbar/menu/menu.tsx
+++ src/components/navbar/menu/menu.tsx
@@ -8,7 +8,7 @@ import type { ReactNode } from 'react';
 
 export const Menu = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='flex flex-col w-full h-[calc(100vh-4.5rem)]'>
+    <div className='flex flex-col w-full h-[calc(100vh-4.4rem)]'>
       <div className='flex justify-end items-center bg-accent px-3 py-1 w-full'>
         {children}
       </div>
