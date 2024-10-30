3b685730c2325a341668cb7ecea9f80eff457414
Falk33n <tim.falk00@gmail.com>
2024-10-30 16:29:41 +0100

    fixed types

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -1,22 +1,38 @@
-import { db } from '@/server/db';
+import { db, type PrismaClientType } from '@/server/db';
 import { initTRPC } from '@trpc/server';
-import { NextRequest } from 'next/server';
+import type {
+  DefaultErrorShape,
+  MiddlewareBuilder,
+  ProcedureBuilder,
+  TRPCError,
+  unsetMarker,
+} from '@trpc/server/unstable-core-do-not-import';
+import type { NextRequest } from 'next/server';
 import superjson from 'superjson';
 import { ZodError } from 'zod';
+
+type CtxProps = { req: NextRequest; resHeaders: Headers; db: PrismaClientType };
+
+type TRPCContextType = Promise<CtxProps>;
 
 export const createTRPCContext = async (opts: {
   req: NextRequest;
   resHeaders: Headers;
-}) => {
+}): TRPCContextType => {
   return {
     db,
     ...opts,
   };
 };
 
+type ErrorProps = {
+  shape: DefaultErrorShape;
+  error: TRPCError;
+};
+
 const t = initTRPC.context<typeof createTRPCContext>().create({
   transformer: superjson,
-  errorFormatter({ shape, error }) {
+  errorFormatter({ shape, error }: ErrorProps) {
     return {
       ...shape,
       data: {
@@ -29,23 +45,43 @@ const t = initTRPC.context<typeof createTRPCContext>().create({
 });
 
 export const createCallerFactory = t.createCallerFactory;
-
 export const createTRPCRouter = t.router;
 
-const timingMiddleware = t.middleware(async ({ next, path }) => {
-  const start = Date.now();
+type TimingMiddlewareType = MiddlewareBuilder<
+  CtxProps,
+  object,
+  object,
+  unknown
+>;
 
-  if (t._config.isDev) {
-    const waitMs = Math.floor(Math.random() * 400) + 100;
-    await new Promise((resolve) => setTimeout(resolve, waitMs));
+const timingMiddleware: TimingMiddlewareType = t.middleware(
+  async ({ next, path }) => {
+    const start: number = Date.now();
+
+    if (t._config.isDev) {
+      const waitMs = Math.floor(Math.random() * 400) + 100;
+      await new Promise((resolve) => setTimeout(resolve, waitMs));
+    }
+
+    const result = await next();
+
+    const end = Date.now();
+    console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
+
+    return result;
   }
+);
 
-  const result = await next();
+type PublicProcedureType = ProcedureBuilder<
+  CtxProps,
+  object,
+  {}, // eslint-disable-line
+  typeof unsetMarker,
+  typeof unsetMarker,
+  typeof unsetMarker,
+  typeof unsetMarker,
+  false
+>;
 
-  const end = Date.now();
-  console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
-
-  return result;
-});
-
-export const publicProcedure = t.procedure.use(timingMiddleware);
+export const publicProcedure: PublicProcedureType =
+  t.procedure.use(timingMiddleware);
