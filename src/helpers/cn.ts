8fa9da40382df60c86205acb3f42a7b0499b7c13
Falk33n <tim.falk00@gmail.com>
2024-10-30 21:06:10 +0100

    added comment

--- src/helpers/cn.ts
+++ src/helpers/cn.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Takes a array of tailwind-css strings and merges them into one single
+ * string, ordered by its initial order.
+ */
 export const cn = (...inputs: ClassValue[]): string => {
   return twMerge(clsx(inputs));
 };
