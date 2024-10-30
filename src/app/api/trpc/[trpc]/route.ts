711b3ebcbef761b9a5b47cab7020c4b4fb489c2c
Falk33n <tim.falk00@gmail.com>
2024-10-30 20:47:28 +0100

    added comments

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,13 +1,20 @@
 import { env } from '@/env';
 import { appRouter } from '@/server/api/root';
-import { createTRPCContext, type CtxProps } from '@/server/api/trpc';
+import { createTRPCContext } from '@/server/api/trpc';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { type NextRequest } from 'next/server';
 
+/**
+ * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
+ * handling a HTTP request (e.g. when you make requests from Client Components).
+ */
 const createContext = async (
   req: NextRequest,
+  /**
+   * `resHeaders` allows response headers to be sent, which allows for creating and setting cookies in the browser.
+   */
   resHeaders: Headers
-): Promise<CtxProps> => {
+) => {
   return createTRPCContext({
     req,
     resHeaders,
