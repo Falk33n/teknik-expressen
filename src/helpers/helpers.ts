31eb231626d0cb28f3da3946215ae016258bb77c
Falk33n <tim.falk00@gmail.com>
2024-10-31 11:12:53 +0100

    fixed typo in comment

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -2,7 +2,7 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 /**
- * Takes a array of tailwind-css strings and merges them into one single
+ * Takes an array of tailwind-css strings and merges them into one single
  * string, ordered by its initial order.
  */
 export const cn = (...inputs: ClassValue[]): string => {
