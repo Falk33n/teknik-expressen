a7b254c670c20965ff13b3f4d3fef9c6e9091252
Falk33n <tim.falk00@gmail.com>
2024-11-09 17:55:40 +0100

    feat: add loading Skeleton to wait for client to render

--- src/app/_components/MenuWrapper.tsx
+++ src/app/_components/MenuWrapper.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { Large_Menu, Small_Menu } from '@/app/_components';
+import { Skeleton } from '@/components';
 import { useMediaQuery } from '@/hooks';
 
 export const MenuWrapper = () => {
   const { mediaQuery } = useMediaQuery();
 
-  if (!mediaQuery) return null;
+  if (!mediaQuery) return <Skeleton className='size-10 lg:w-[560px]' />;
   else if (mediaQuery === 'xs' || mediaQuery === 'sm' || mediaQuery === 'md')
     return <Small_Menu />;
   return <Large_Menu />;
--- src/app/_components/ThemeProvider.tsx
+++ src/app/_components/ThemeProvider.tsx
@@ -1,32 +1,9 @@
-'use client';
-
-import { Skeleton } from '@/components';
 import {
   ThemeProvider as Next_ThemeProvider,
   type ThemeProviderProps,
 } from 'next-themes';
-import { useEffect, useState } from 'react';
 
-export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  return (
-    <Next_ThemeProvider {...props}>
-      {children}
-
-      {!isMounted && (
-        <div
-          aria-hidden
-          className='fixed top-0 z-[200] h-screen w-screen bg-background'
-        >
-          <Skeleton className='size-full rounded-none' />
-        </div>
-      )}
-    </Next_ThemeProvider>
-  );
-};
+export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => (
+  <Next_ThemeProvider {...props}>{children}</Next_ThemeProvider>
+);
 ThemeProvider.displayName = 'ThemeProvider';
