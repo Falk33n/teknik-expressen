fdc5f426e88ca022ad8c9552adced676a446b750
Falk33n <tim.falk00@gmail.com>
2024-11-09 18:22:20 +0100

    perf: call query from parent to not re-call it from both

--- src/app/_components/Large_Menu.tsx
+++ src/app/_components/Large_Menu.tsx
@@ -1,45 +1,38 @@
-'use client';
-
 import { Large_MenuProducts, ThemeToggle } from '@/app/_components';
 import { Link } from '@/components';
-import { api } from '@/trpc';
 
-export const Large_Menu = () => {
-  const { data, isLoading } = api.session.getSession.useQuery(undefined, {
-    retry: false,
-  });
-
-  const isAuthenticated = data && data.isAuthenticated && !isLoading;
-
-  return (
-    <ul className='flex items-center gap-6 whitespace-nowrap xl:gap-10'>
-      <li>
-        <Large_MenuProducts />
-      </li>
-      <li>
-        <Link className='rounded-sm px-4 py-2' href='/support'>
-          Kundtjänst
-        </Link>
-      </li>
-      <li>
-        <Link
-          className='rounded-sm px-4 py-2'
-          aria-live='polite'
-          aria-label={`Gå till sidan ${isAuthenticated ? 'Mitt konto' : 'Logga in'}`}
-          href={isAuthenticated ? '/account' : '/login'}
-        >
-          {isAuthenticated ? 'Mitt konto' : 'Logga in'}
-        </Link>
-      </li>
-      <li>
-        <Link className='rounded-sm px-4 py-2' href='/cart'>
-          Kundkorg
-        </Link>
-      </li>
-      <li>
-        <ThemeToggle />
-      </li>
-    </ul>
-  );
-};
+export const Large_Menu = ({
+  isAuthenticated,
+}: {
+  isAuthenticated?: boolean;
+}) => (
+  <ul className='flex items-center gap-6 whitespace-nowrap xl:gap-10'>
+    <li>
+      <Large_MenuProducts />
+    </li>
+    <li>
+      <Link className='rounded-sm px-4 py-2' href='/support'>
+        Kundtjänst
+      </Link>
+    </li>
+    <li>
+      <Link
+        className='rounded-sm px-4 py-2'
+        aria-live='polite'
+        aria-label={`Gå till sidan ${isAuthenticated ? 'Mitt konto' : 'Logga in'}`}
+        href={isAuthenticated ? '/account' : '/login'}
+      >
+        {isAuthenticated ? 'Mitt konto' : 'Logga in'}
+      </Link>
+    </li>
+    <li>
+      <Link className='rounded-sm px-4 py-2' href='/cart'>
+        Kundkorg
+      </Link>
+    </li>
+    <li>
+      <ThemeToggle />
+    </li>
+  </ul>
+);
 Large_Menu.displayName = 'Large_Menu';
--- src/app/_components/MenuWrapper.tsx
+++ src/app/_components/MenuWrapper.tsx
@@ -3,14 +3,21 @@
 import { Large_Menu, Small_Menu } from '@/app/_components';
 import { Skeleton } from '@/components';
 import { useMediaQuery } from '@/hooks';
+import { api } from '@/trpc';
 
 export const MenuWrapper = () => {
   const { mediaQuery } = useMediaQuery();
 
-  if (!mediaQuery)
+  const { data, isLoading } = api.session.getSession.useQuery(undefined, {
+    retry: false,
+  });
+
+  const isAuthenticated = data && data.isAuthenticated;
+
+  if (!mediaQuery || isLoading)
     return <Skeleton className='size-10 lg:w-[560px] xl:w-[620px]' />;
   else if (mediaQuery === 'xs' || mediaQuery === 'sm' || mediaQuery === 'md')
-    return <Small_Menu />;
-  return <Large_Menu />;
+    return <Small_Menu isAuthenticated={isAuthenticated} />;
+  return <Large_Menu isAuthenticated={isAuthenticated} />;
 };
 MenuWrapper.displayName = 'MenuWrapper';
