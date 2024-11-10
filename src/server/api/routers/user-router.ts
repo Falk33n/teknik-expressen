45ebaeac75b5ad1b64424b1479b9a63effdad082
Falk33n <tim.falk00@gmail.com>
2024-11-10 22:07:23 +0100

    feat: add try catch block to handle errors more gracefully

--- src/server/api/routers/user-router.ts
+++ src/server/api/routers/user-router.ts
@@ -1,4 +1,9 @@
-import { ConflictError, generateSaltHash, getCookieConsent } from '@/lib';
+import {
+  ConflictError,
+  generateSaltHash,
+  getCookieConsent,
+  InternalServerError,
+} from '@/lib';
 import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
 import { z } from 'zod';
 
@@ -23,51 +28,67 @@ export const userRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      const [existingEmail, existingPhoneNumber] = await Promise.all([
-        ctx.db.user.findUnique({ where: { email: input.email } }),
-        ctx.db.user.findUnique({ where: { phoneNumber: input.phoneNumber } }),
-      ]);
-      if (existingEmail || existingPhoneNumber) {
-        throw new ConflictError(
-          'E-postadress eller telefonnummer används redan',
-        );
+      try {
+        const [existingEmail, existingPhoneNumber] = await Promise.all([
+          ctx.db.user.findUnique({ where: { email: input.email } }),
+          ctx.db.user.findUnique({ where: { phoneNumber: input.phoneNumber } }),
+        ]);
+        if (existingEmail || existingPhoneNumber) {
+          throw new ConflictError(
+            'E-postadress eller telefonnummer används redan',
+          );
+        }
+
+        const newUser = await ctx.db.user.create({
+          data: {
+            fullName: input.fullName,
+            streetAddress: input.streetAddress,
+            postalCode: input.postalCode.replace(/\s+/g, ''),
+            city: input.city,
+            country: input.country,
+            email: input.email,
+            phoneNumber: input.phoneNumber,
+          },
+        });
+
+        const { salt, hashedPassword } = await generateSaltHash(input.password);
+
+        const consent = getCookieConsent(ctx.req);
+        const isConsentGiven = consent.status !== 204 ? true : false;
+
+        await Promise.all([
+          ctx.db.password.create({
+            data: {
+              userId: newUser.id,
+              salt,
+              hashedPassword,
+            },
+          }),
+          ctx.db.cookieConsent.create({
+            data: {
+              userId: newUser.id,
+              consentGiven: isConsentGiven,
+            },
+          }),
+        ]);
+
+        return {
+          status: 200,
+          message: 'Lyckades! Användaren är skapad',
+          isUserCreated: true,
+        };
+      } catch (error) {
+        if (error instanceof ConflictError) {
+          return {
+            status: 409,
+            message: `Misslyckades! ${error.message}`,
+            isUserCreated: false,
+          };
+        } else if (!(error instanceof InternalServerError)) {
+          throw new InternalServerError();
+        }
+
+        throw error;
       }
-
-      const newUser = await ctx.db.user.create({
-        data: {
-          fullName: input.fullName,
-          streetAddress: input.streetAddress,
-          postalCode: input.postalCode.replace(/\s+/g, ''),
-          city: input.city,
-          country: input.country,
-          email: input.email,
-          phoneNumber: input.phoneNumber,
-        },
-      });
-
-      const { salt, hashedPassword } = await generateSaltHash(input.password);
-
-      const consent = getCookieConsent(ctx.req);
-      const isConsentGiven = consent !== null && consent ? true : false;
-
-      await Promise.all([
-        ctx.db.password.create({
-          data: {
-            userId: newUser.id,
-            salt,
-            hashedPassword,
-          },
-        }),
-        ctx.db.cookieConsent.create({
-          data: {
-            userId: newUser.id,
-            consentGiven: isConsentGiven,
-          },
-        }),
-      ]);
-
-      return {
-        message: 'Lyckades skapa användaren',
-      };
     }),
 });
