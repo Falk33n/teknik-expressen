98ae0240d19266517296957cfe5e68491d78923b
Falk33n <tim.falk00@gmail.com>
2024-11-10 22:27:30 +0100

    fix: change amount of box shadow

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -2,7 +2,7 @@ import { MenuWrapper, Searchbar } from '@/app/_components';
 
 export const Navbar = () => (
   <nav
-    className='sticky top-0 z-[20] bg-accent/20 shadow-md backdrop-blur-lg'
+    className='sticky top-0 z-[20] bg-accent/20 shadow backdrop-blur-lg'
     aria-label='Huvudnavigation'
   >
     <div className='mx-auto flex w-full items-center justify-between gap-6 p-4 sm:gap-8 sm:px-12 sm:py-5 md:gap-10 lg:w-[90vw] xl:w-[75vw]'>
