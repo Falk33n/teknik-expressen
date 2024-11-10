a1dbd801de4a09955e443199ff6c26581248868f
Falk33n <tim.falk00@gmail.com>
2024-11-10 13:04:06 +0100

    feat: add statusCode to show it in error boundaries

--- src/lib/exceptions.ts
+++ src/lib/exceptions.ts
@@ -1,31 +1,40 @@
 import { TRPCError } from '@trpc/server';
 
 export class ConflictError extends TRPCError {
+  public statusCode: number;
+
   constructor(message: string) {
     super({
       code: 'CONFLICT',
       message,
     });
     this.name = 'ConflictError';
+    this.statusCode = 409;
   }
 }
 
 export class UnauthorizedError extends TRPCError {
+  public statusCode: number;
+
   constructor(message: string) {
     super({
       code: 'UNAUTHORIZED',
       message,
     });
     this.name = 'UnauthorizedError';
+    this.statusCode = 401;
   }
 }
 
 export class InternalServerError extends TRPCError {
+  public statusCode: number;
+
   constructor(message: string = 'Något gick fel') {
     super({
       code: 'INTERNAL_SERVER_ERROR',
       message,
     });
     this.name = 'InternalServerError';
+    this.statusCode = 500;
   }
 }
