d3ea2063c91ad1a9a4149188ea53c60109cd8716
Falk33n <tim.falk00@gmail.com>
2024-11-10 22:06:16 +0100

    feat: add try catch to handle errors more gracefully

--- src/server/api/routers/session-router.ts
+++ src/server/api/routers/session-router.ts
@@ -1,6 +1,7 @@
 import {
   getSecretJwtKey,
   getSession,
+  InternalServerError,
   UnauthorizedError,
   verifyPassword,
 } from '@/lib';
@@ -22,58 +23,74 @@ export const sessionRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      const user = await ctx.db.user.findUnique({
-        where: {
-          email: input.email,
-        },
-      });
-      if (!user) {
-        throw new UnauthorizedError('Felaktig e-postadress eller lösenord');
-      }
+      try {
+        const user = await ctx.db.user.findUnique({
+          where: {
+            email: input.email,
+          },
+        });
+        if (!user) {
+          throw new UnauthorizedError('Felaktig e-postadress eller lösenord');
+        }
 
-      const passwordEntry = await ctx.db.password.findUnique({
-        where: { userId: user.id },
-      });
-      if (!passwordEntry) {
-        throw new UnauthorizedError('Felaktig e-postadress eller lösenord');
-      }
+        const passwordEntry = await ctx.db.password.findUnique({
+          where: { userId: user.id },
+        });
+        if (!passwordEntry) {
+          throw new UnauthorizedError('Felaktig e-postadress eller lösenord');
+        }
 
-      const isValidPassword = await verifyPassword(
-        input.password,
-        passwordEntry.salt,
-        passwordEntry.hashedPassword,
-      );
-      if (!isValidPassword) {
-        throw new UnauthorizedError('Felaktig e-postadress eller lösenord');
-      }
+        const isValidPassword = await verifyPassword(
+          input.password,
+          passwordEntry.salt,
+          passwordEntry.hashedPassword,
+        );
+        if (!isValidPassword) {
+          throw new UnauthorizedError('Felaktig e-postadress eller lösenord');
+        }
 
-      const tokenExpiration = input.rememberMe ? '30d' : '2h';
-      const cookieExpiration = input.rememberMe
-        ? 30 * 24 * 60 * 60
-        : 2 * 60 * 60;
+        const tokenExpiration = input.rememberMe ? '30d' : '2h';
+        const cookieExpiration = input.rememberMe
+          ? 30 * 24 * 60 * 60
+          : 2 * 60 * 60;
 
-      const { jwtKey } = getSecretJwtKey();
-      const jwtToken = await new SignJWT({
-        userId: user.id,
-      })
-        .setProtectedHeader({
-          alg: 'HS256',
+        const { jwtKey } = getSecretJwtKey();
+        const jwtToken = await new SignJWT({
+          userId: user.id,
         })
-        .setExpirationTime(tokenExpiration)
-        .sign(jwtKey);
+          .setProtectedHeader({
+            alg: 'HS256',
+          })
+          .setExpirationTime(tokenExpiration)
+          .sign(jwtKey);
 
-      const sessionCookie = serialize('sc', jwtToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict',
-        maxAge: cookieExpiration,
-        path: '/',
-      });
-      ctx.resHeaders.set('Set-Cookie', sessionCookie);
+        const sessionCookie = serialize('sc', jwtToken, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'strict',
+          maxAge: cookieExpiration,
+          path: '/',
+        });
+        ctx.resHeaders.set('Set-Cookie', sessionCookie);
 
-      return {
-        message: 'Lyckades skapa en session',
-      };
+        return {
+          status: 200,
+          message: 'Lyckades! Session skapad',
+          isSessionCreated: true,
+        };
+      } catch (error) {
+        if (error instanceof UnauthorizedError) {
+          return {
+            status: 401,
+            message: `Misslyckades! ${error.message}`,
+            isSessionCreated: false,
+          };
+        } else if (!(error instanceof InternalServerError)) {
+          throw new InternalServerError();
+        }
+
+        throw error;
+      }
     }),
 
   getSession: publicProcedure.query(
