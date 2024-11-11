ddd673d57d60db49474c648fdb4e00262cf5c54d
Falk33n <tim.falk00@gmail.com>
2024-11-11 20:55:33 +0100

    refactor: remove supressHydrationWarning

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,14 +1,28 @@
+'use client';
+
 import Error from '@/app/error';
-import RootLayout from '@/app/layout';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+  style: ['italic', 'normal'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+  preload: true,
+});
 
 type ErrorProps = {
   error: Error & { digest?: string };
 };
 
 const GlobalError = ({ error }: ErrorProps) => (
-  <RootLayout>
-    <Error error={error} />
-  </RootLayout>
+  <html lang='sv'>
+    <body className={`${inter.className} w-full`}>
+      <main className='mx-auto flex w-full flex-col bg-background lg:w-[90vw] xl:w-[75vw]'>
+        <Error error={error} />
+      </main>
+    </body>
+  </html>
 );
 GlobalError.displayName = 'GlobalError';
 
