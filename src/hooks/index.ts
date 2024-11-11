82fe0a517d6fd30d5e05acb1b9832f49b1f615b6
Falk33n <tim.falk00@gmail.com>
2024-11-11 21:50:52 +0100

    feat: remove unnecessary hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,3 @@
-export { useCreateSession } from './auth-hooks';
 export { useErrorLog } from './log-hooks';
 export { useMediaQuery } from './media-query-hooks';
 export { reducer, toast, useToast } from './toast-hooks';
-export { useCreateUser } from './user-hooks';
