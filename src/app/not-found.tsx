6f8eed825aeb93c1dd1e545b2fad2b43a37fd077
Falk33n <tim.falk00@gmail.com>
2024-11-12 15:40:06 +0100

    refactor: change import path for error specific components

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ErrorButton, ErrorContainer } from '@/components';
+import { ErrorButton, ErrorContainer } from '@/app/_components';
 import { usePathname } from 'next/navigation';
 
 const NotFound = () => {
