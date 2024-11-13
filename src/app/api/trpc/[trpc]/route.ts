f5347c15bd4a6eb5167c26e8789db3936a0b319f
Falk33n <tim.falk00@gmail.com>
2024-11-13 18:01:51 +0100

    refactor: change import path

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,5 +1,5 @@
 import { env } from '@/env';
-import { appRouter } from '@/server/api';
+import { appRouter } from '@/server';
 import { createTRPCContext } from '@/server/api/trpc';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import type { NextRequest } from 'next/server';
