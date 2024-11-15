a990c4e9b09935dbbacb84ecf1a9f9aab29a598a
Falk33n <tim.falk00@gmail.com>
2024-11-15 20:26:12 +0100

    feat: add logo component

---
+++ src/components/ui/Logo.tsx
@@ -0,0 +1,60 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/feedback';
+import { Link } from '@/components/ui';
+import { cn } from '@/lib/utils';
+import { Moul } from 'next/font/google';
+
+const moul = Moul({
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+  style: 'normal',
+  weight: '400',
+  subsets: ['latin'],
+  preload: true,
+});
+
+type LogoProps = {
+  className?: string;
+};
+
+export const Logo = ({ className }: LogoProps) => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <h1>
+          <Link
+            href='/'
+            className={cn(
+              '-ml-2 rounded-md p-2 text-foreground/80',
+              moul.className,
+              className,
+            )}
+          >
+            TeknikExpressen
+          </Link>
+        </h1>
+      </TooltipTrigger>
+
+      <TooltipContent aria-hidden side='bottom' sideOffset={16}>
+        <p>Gå till startsidan</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
+Logo.displayName = 'Logo';
+
+export const LogoReadonly = ({ className }: LogoProps) => (
+  <h1
+    className={cn(
+      'text-2xl text-foreground/80 md:text-3xl lg:text-4xl',
+      moul.className,
+      className,
+    )}
+  >
+    TeknikExpressen
+  </h1>
+);
+LogoReadonly.displayName = 'LogoReadonly';
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,6 @@
 export * from './Accordion';
 export * from './IconWithText';
 export * from './Link';
+export * from './Logo';
 export * from './Separator';
 export * from './Skeleton';
