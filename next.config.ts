053bcf5e0d34270370a83b880bbe7e4743619507
Falk33n <tim.falk00@gmail.com>
2024-11-15 20:45:49 +0100

    chore: remove comments

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /**
-   * Specify your server-side environment variables here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
   serverRuntimeConfig: {
     SECRET_DATABASE_URL: process.env.SECRET_DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     SECRET_JWT_KEY: process.env.SECRET_JWT_KEY,
-  },
-  /**
-   * Specify your client-side environment variables here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
-  publicRuntimeConfig: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
 };
 export default nextConfig;
--- src/env.js
+++ src/env.js
@@ -2,10 +2,6 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 export const env = createEnv({
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
   server: {
     SECRET_DATABASE_URL: z.string().url(),
     NODE_ENV: z
@@ -13,32 +9,11 @@ export const env = createEnv({
       .default('development'),
     SECRET_JWT_KEY: z.string(),
   },
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
-  client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
-  },
-  /**
-   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-   * middlewares) or client-side so we need to destruct manually.
-   */
   runtimeEnv: {
     SECRET_DATABASE_URL: process.env.SECRET_DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     SECRET_JWT_KEY: process.env.SECRET_JWT_KEY,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-   * useful for Docker builds.
-   */
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  /**
-   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-   * `SOME_VAR=''` will throw an error.
-   */
   emptyStringAsUndefined: !!process.env.EMPTY_ENV_STRING_UNDEFINED,
 });
