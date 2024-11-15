82741badaf29868eecf703ec4f656ab5ac2716d2
Falk33n <tim.falk00@gmail.com>
2024-11-15 20:05:26 +0100

    refactor: make function of router switch to make code more maintainable

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,41 +1,12 @@
-import { getSession } from '@/lib';
-import { type NextRequest, NextResponse } from 'next/server';
+import { getSession, handleProtectedRouteMiddleware } from '@/lib/utils';
+import type { NextRequest } from 'next/server';
 
 export const middleware = async (req: NextRequest) => {
-  const { status } = await getSession(req, false);
-
+  const { status } = await getSession(false);
   const isAuthenticated = status === 'success';
-
-  switch (req.nextUrl.pathname) {
-    case '/register': {
-      if (isAuthenticated) {
-        return NextResponse.redirect(new URL('/account', req.url));
-      }
-      break;
-    }
-    case '/login': {
-      if (isAuthenticated) {
-        return NextResponse.redirect(new URL('/account', req.url));
-      }
-      break;
-    }
-    case '/account': {
-      if (!isAuthenticated) {
-        return NextResponse.redirect(new URL('/login', req.url));
-      }
-      break;
-    }
-    default: {
-      return NextResponse.next();
-    }
-  }
+  return handleProtectedRouteMiddleware(req, isAuthenticated);
 };
 
 export const config = {
-  matcher: [
-    '/account/:path*',
-    '/login/:path*',
-    '/register/:path*',
-    '/dashboard/:path*',
-  ],
+  matcher: ['/account/:path*', '/login/:path*', '/register/:path*'],
 };
