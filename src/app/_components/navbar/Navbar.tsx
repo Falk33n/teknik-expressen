f5bc6e3dfd6f04e86c8d9e2c60ebdf1e3b9ba69e
Falk33n <tim.falk00@gmail.com>
2024-11-15 20:19:19 +0100

    refactor: add a extra searchbar that will get removed on larger screens

--- src/app/_components/navbar/Navbar.tsx
+++ src/app/_components/navbar/Navbar.tsx
@@ -1,20 +1,24 @@
-import { MenuWrapper, Searchbar } from '@/app/components';
+import { MenuWrapper } from '@/app/components/menu';
+import { Searchbar } from '@/app/components/navbar';
+import type { HasActiveSession } from '@/app/layout';
+import { Logo } from '@/components/ui';
 
-export const Navbar = () => (
+export const Navbar = ({ hasActiveSession }: HasActiveSession) => (
   <nav
     className='sticky top-0 z-[20] bg-accent/20 shadow backdrop-blur-lg'
     aria-label='Huvudnavigation'
   >
     <div className='mx-auto flex w-full items-center justify-between gap-6 p-4 sm:gap-8 sm:px-12 sm:py-5 md:gap-10 lg:w-[90vw] xl:w-[75vw]'>
       <div className='flex flex-1 items-center justify-between gap-6 sm:gap-8 md:justify-start md:gap-10'>
-        {/* TODO: place brand logo here */}
-        <p>logo</p>
+        <Logo />
 
-        <Searchbar />
+        <Searchbar className='max-sm:hidden' />
       </div>
 
-      <MenuWrapper />
+      <MenuWrapper hasActiveSession={hasActiveSession} />
     </div>
+
+    <Searchbar className='sm:hidden' />
   </nav>
 );
 Navbar.displayName = 'Navbar';
